1320d70d540cccc5109881e789c3c7b46e31e9d1
ducanh96 <rogerducanh@gmail.com>
2021-10-13 20:51:21 +0700

    Add project files.

---
+++ DucAnh.IDP/Config.cs
@@ -0,0 +1,82 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DucAnh.IDP
+{
+    public static class Config
+    {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>()
+            {
+                new TestUser
+                {
+                    SubjectId = "d860efca-22d9-47fd-8249-791ba61b07c7",
+                    Username = "Frank",
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Frank"),
+                        new Claim("family_name", "Underwood"),
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "b7539694-97e7-4dfe-84da-b4256e1ff5c7",
+                    Username = "Claire",
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Claire"),
+                        new Claim("family_name", "Underwood"),
+                    }
+                }
+            };
+        }
+
+        // identity-related resources (scopes)
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>()
+            {
+                new Client
+                {
+                    ClientName = "duc anh demo",
+                    ClientId = "ducanhdemo",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    RedirectUris = new List<string>()
+                    {
+                        "https://localhost:44385/signin-oidc",
+                        "http://localhost:5000/signin-oidc",
+
+                    },
+                    AllowedScopes = new List<string>()
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId
+                    },
+                    ClientSecrets = new List<Secret>()
+                    {
+                        new Secret("secret".Sha256())
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ src/ImageGallery.Client/Startup.cs
@@ -0,0 +1,69 @@
+﻿using ImageGallery.Client.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ImageGallery.Client
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+ 
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc(opt => opt.EnableEndpointRouting = false);
+
+            // register an IHttpContextAccessor so we can access the current
+            // HttpContext in services by injecting it
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            // register an IImageGalleryHttpClient
+            services.AddScoped<IImageGalleryHttpClient, ImageGalleryHttpClient>();
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultScheme = "Cookies";
+                opt.DefaultChallengeScheme = "oidc";
+            }).AddCookie("Cookies")
+              .AddOpenIdConnect("oidc", opt =>
+              {
+                  opt.SignInScheme = "Cookies";
+                  opt.Authority = "https://localhost:44385";
+                  opt.ClientId = "ducanhdemo";
+                  opt.ResponseType = "code id_token";
+                  opt.Scope.Add("openid");
+                  opt.Scope.Add("profile");
+                  opt.SaveTokens = true;
+                  opt.ClientSecret = "secret";
+              });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Shared/Error");
+            }
+
+            app.UseAuthentication(); // <-- Add it here.
+            app.UseStaticFiles();
+            app.UseMvc();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
